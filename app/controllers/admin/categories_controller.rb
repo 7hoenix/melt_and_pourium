35f4268935601484d140637a6777d6f3acd0f1cf
philmphoenix <jpholzmann@gmail.com>
2015-10-27 07:30:34 -0600

    Adds create functionality to controller
    Now a category can be created in admin namespaced controller.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,4 +2,21 @@ class Admin::CategoriesController < AdminController
   def new
     @category = Category.new
   end
+
+  def create
+    category = Category.new(category_params)
+    if category.save
+      flash[:notice] = "Successfully created category"
+      redirect_to categories_path
+    else
+      flash[:errors] = "Invalid category params"
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :image_url)
+  end
 end
