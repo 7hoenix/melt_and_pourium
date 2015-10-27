6b2b62dc48949eff971755f2c9a90ea91a12e117
philmphoenix <jpholzmann@gmail.com>
2015-10-27 07:02:31 -0600

    Adds acceptance test for category creation
    This test does not address paperclip.

---
+++ spec/features/admin_can_create_new_categories_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+RSpec.feature "Admin can" do
+
+  def create_admin
+    User.create(email: "admin@example.com", password: "password", role: 3)
+  end
+
+  def admin_logs_in
+    create_admin
+    visit login_path
+    fill_in("Email", with: "admin@example.com")
+    fill_in("Password", with: "password")
+    within(:css, "div#login_form") do
+      click_on("Login")
+    end
+  end
+
+  scenario "create new categories" do
+    create_admin
+    admin_logs_in
+
+    click_on "Create New Category"
+    fill_in("Name", with: "Gems")
+    fill_in("Description", with: "Cool looking gems")
+    click_on "Create Category"
+
+    expect(current_path).to eq(categories_path)
+    expect(page).to have_content("Gems")
+  end
+end
