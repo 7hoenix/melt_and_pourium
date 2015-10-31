c89778df6cc11aeed11ae3a55d3646f13f965e57
philmphoenix <jpholzmann@gmail.com>
2015-10-31 11:24:21 -0600

    Adds edit update and destroy functionality
    Now an admin can edit update and destroy a category.

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < AdminController
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def new
     @category = Category.new
   end
@@ -14,7 +16,29 @@ class Admin::CategoriesController < AdminController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Successfully Updated"
+      redirect_to categories_path
+    else
+      flash[:errors] = "Invalid update params"
+      redirect_to edit_admin_category_path(@category)
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to categories_path
+  end
+
   private
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name, :image_url)
